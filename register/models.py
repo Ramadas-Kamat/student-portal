328325c3151185a0098d9f7b96f670571d8f6feb
Ramadas Kamat <ramadasramadas7@gmail.com>
2020-12-06 04:09:39 +0530

    Models created

--- register/models.py
+++ register/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=25)
+    usn = models.CharField(max_length=10,primary_key=True)
+    address = models.TextField()
+    sem = models.IntegerField(default=1)
+    classrm = models.ForeignKey('Classroom',null=True,blank=False,\
+        on_delete=models.CASCADE)
+
+class Faculty(models.Model):
+    name = models.CharField(max_length=25)
+    ssn = models.CharField(max_length=10,primary_key=True)
+    address = models.TextField()
+    dept = models.CharField(max_length=10)
+
+class Course(models.Model):
+    name = models.CharField(max_length=20)
+    sem = models.IntegerField()
+
+class Classroom(models.Model):
+    name = models.CharField(max_length=10)
+    dept = models.CharField(max_length=10)
+    sem = models.IntegerField(default=1)
+    course = models.ManyToManyField(Course)
