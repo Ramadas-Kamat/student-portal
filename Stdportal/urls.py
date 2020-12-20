e2cac5a9bff7fde47706ddf34b3c4df6ef830f80
Shravan R Tandel <shravansrt10@gmail.com>
2020-12-20 19:46:54 +0530

    Registered models on admin.py and updated urls.py

--- Stdportal/urls.py
+++ Stdportal/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from register import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('register.urls'))
+    path('', views.index, name = 'index'),
+    path('register', views.register, name = 'register'),
 ]
--- register/admin.py
+++ register/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import Student, TimeTable, AutomateRegister
 
 # Register your models here.
+
+admin.site.register(Student)
+admin.site.register(TimeTable)
+admin.site.register(AutomateRegister)
